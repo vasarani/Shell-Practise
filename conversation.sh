4e97e1e32fd2d46f7d356d32b3a844359186011e
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-01-30 07:54:30 +0530

    Conversation messages

---
+++ conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Rani: Hi Ram, How are you?"
+echo "Ram: Hey hi, I'm fine! What about you?"
+echo "Rani: yeah I'm good, good to see you today"
+echo "Ram: Really! so nice that we meet again after long time."
