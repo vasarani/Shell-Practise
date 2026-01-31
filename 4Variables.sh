7e3e3a5e05f07c414ea30c21ef4d15bd493e97d7
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-01-31 15:41:44 +0530

    $ Uses

--- 4Variables.sh
+++ 4Variables.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-TIMESTAMP=$(date)
+STARTTIME=$(date)
 echo "Script Executed at: $TIMESTAMP"
+
+sleep 10
+
+ENDTIME=$(date +%s)
+TOTAL_TIME=$(($ENDTIME-$STARTTIME))
+
+echo "Script executed in: $TOTAL_TIME"
