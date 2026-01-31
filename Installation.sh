5700e7b666c516374b3eaf4d62752cd04ccf42ce
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-01-31 20:51:40 +0530

    Added

--- Installation.sh
+++ Installation.sh
@@ -7,3 +7,10 @@ if [$USERID -ne 0]; then
 fi 
  echo "Installing Nginx"
  dnf install nginx -y
+
+if [ $? -ne 0 ]; then 
+ echo "Installing Nginx... FAILURE"
+ exit 1
+else  
+   echo "Installing Nginx... SUCCESS"
+fi
