59644fadded1c1b1a703e411c87868162e2d7d6b
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-01-31 17:55:39 +0530

    message

--- conditions.sh
+++ conditions.sh
@@ -7,6 +7,9 @@ NUMBER=$1
 # -ne --> not equal
 if [ $NUMBER -gt 20 ]; then
   echo "Given number: $NUMBER is greater than 20"
+
+elif [$NUMER -eq 20]; then
+ echo "Given number: $NUMBER is equal 20"
 else
  echo "Given number: $NUMBER is less thn 20"
 
