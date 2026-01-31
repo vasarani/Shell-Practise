ffc93082ec730a1a1120f2ca1f4802dd68ef43ec
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-01-31 17:23:44 +0530

    $ Uses

--- data-types.sh
+++ data-types.sh
@@ -8,5 +8,5 @@ echo "sum is : $SUM"
 FRUITS=("Apple" "Banana" "Pomogranate")
 echo "Fruites are: ${FRUITS[@]}" 
 echo "First Fruit is: ${FRUITS[0]}"
-echo "Second fruit is: ${FRUIT[1]}"
-echo "Third fruit is: ${FRUIT[2]}"
+echo "Second fruit is: ${FRUITS[1]}"
+echo "Third fruit is: ${FRUITS[2]}"
