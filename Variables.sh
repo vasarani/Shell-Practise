47faff4a3ec8c36731dbb7e15993175a7de447ea
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-01-31 14:56:35 +0530

    Variables

---
+++ Variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+Person1=Rani
+Person2=Ram
+
+echo "$Person1: Hi $Person2, How are you?"
+echo "$Person2: Hey hi, I'm fine! What about you?"
+echo "$Person1: yeah I'm good, good to see you today"
+echo "$Person2: Really! so nice that we meet again after long time."
