cf43c007b9766eaa5739292c81eabb6e098be102
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-02-01 07:36:28 +0530

    New Changes added

---
+++ 2loops.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shellscript"
+LOGS_FILE="/var/log/shellscript/$0.log"
+
+
+if [$USERID -ne 0]; then
+ echo "Please run this script with root user access" | tee -a $LOGS_FILE
+ exit 1
+fi 
+
+mkdir -p $LOGS_FOLDERS
+
+VALIDATE(){
+
+if [ $1 -ne 0 ]; then
+ echo "$2.... FAILURE" | tee -a $LOGS_FILE
+ exit 1
+else
+ echo  "$2.... SUCCESS" | tee -a $LOGS_FILE
+fi
+
+}
+
+for package in $@ #sudo sh 1loops.sh nginx mysql nodejs
+
+do
+ dnf list installed $package &>>$LOGS_FILE
+ if [ $? -ne 0 ]; then
+  echo  "$package not installed, Installing now"
+
+  dnf install $package -y &>>LOGS_FILE
+  VALIDATE $? "$package installation"
+ else 
+   echo "$package already installed, Skipping"
+   
+ fi
+
+done
