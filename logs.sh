ca641b19ba3260e8e59313f85b92089c917e6fce
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-02-01 06:33:29 +0530

    New Changes added

---
+++ logs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shellscript"
+LOGS_FILE="/var/log/shellscript/$0.log"
+
+
+if [$USERID -ne 0]; then
+ echo "Please run this script with root user access"
+ exit 1
+fi 
+
+mkdir -p $LOGS_FOLDERS
+
+VALIDATE(){
+
+if [ $1 -ne 0 ]; then
+ echo "$2.... FAILURE"
+ exit 1
+else
+ echo  "$2.... SUCCESS"
+fi
+
+}
+
+dnf install nginx -y &>> $LOGS_FILE
+VALIDATE $? "Nginx Installation"
+
+dnf install mysql -y &>> $LOGS_FILE
+VALIDATE $? "MySQL Installation"
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Nodejs Installation"
