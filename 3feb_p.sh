6ab324a29bc3198b874a998e54516ca0747e2fd2
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-02-02 12:09:36 +0530

    .....

--- 3feb_p.sh
+++ 3feb_p.sh
@@ -4,7 +4,7 @@ LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
- echo "Please run this script with root user access"
+ echo "Please run this script with root user access" | tee -a $LOGS_FILE
  exit 1
 fi
 
@@ -13,15 +13,15 @@ mkdir -p $LOGS_FOLDER
 #By default shell will not execute, only executed when called
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-     echo "$2.... FAILURE"
+     echo "$2.... FAILURE" | tee -a $LOGS_FILE
      exit 1
     else
-     echo "$2.... SUCCESS"
+     echo "$2.... SUCCESS" | tee -a $LOGS_FILE
     fi  
 }
 echo "Installing Nginx"
 dnf install nginx -y &>> $LOGS_FILE # this will redirects the output to logs
-VALIDATE $? "Nginx Installation"
+VALIDATE $? "Nginx Installation" 
 
 echo "Installing MySQL"
 dnf install mysql -y &>> $LOGS_FILE
