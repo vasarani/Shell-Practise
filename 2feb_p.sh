c65cc9762a7e7183fd7c60bdb74d33489547ecd8
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-02-02 11:18:56 +0530

    Changes are added

---
+++ 2feb_p.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+ echo "Please run this script with root user access"
+ exit 1
+fi
+#By default shell will not execute, only executed when called
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+     echo "$2.... FAILURE"
+     exit 1
+    else
+     echo "$2.... SUCCESS"
+    fi  
+}
+echo "Installing Nginx"
+dnf install nginx -y
+VALIDATE $? "Nginx Installation"
+
+echo "Installing MySQL"
+dnf install mysql -y
+VALIDATE $? "MySQL Installation"
+
+echo "Installing Nodejs"
+dnf install nodejs -y
+VALIDATE $? "Nodejs Installation"
